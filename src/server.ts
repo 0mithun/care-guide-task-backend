2498bff60ac89c6c1d23e4f80f4275c2a6aae8a0
Mithun Halder <mithunrptc@gmail.com>
2026-08-08 19:57:12 +0600

    update server.ts for vercel deployment

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { connectDB } from './config/database';
 
@@ -17,9 +17,17 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    res.status(500).json({ error: 'Database connection failed' });
+  }
+});
+
 app.use('/api-docs', swaggerRouter);
-
-
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
@@ -29,14 +37,14 @@ app.get('/health', (req: Request, res: Response) => {
   res.status(200).json({ status: 'OK', timestamp: new Date() });
 });
 
-
 app.get('/', (req: Request, res: Response) => {
-  res.send('Secure Note-Taking API is running. Documentation is available at /api-docs');
+  res.send('API is running.');
 });
 
+if (process.env.NODE_ENV != 'production') {
+  app.listen(PORT, ()=> {
+    console.log(`Server running locally on port ${PORT}`)
+  })
+}
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+export default app
